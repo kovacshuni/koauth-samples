c28058241470bca96f3efe2a632ef18973875df1
Hunor Kovács <hunor.kovacs@ft.com>
2015-06-05 11:48:14 +0300

    Preparing release 1.1.0

--- scala-consumer-scalatra/project/build.scala
+++ scala-consumer-scalatra/project/build.scala
@@ -5,7 +5,7 @@ import org.scalatra.sbt._
 object KoauthsamplescalatraBuild extends Build {
   val Organization = "com.hunorkovacs"
   val Name = "koauth-sample-scala-consumer-scalatra"
-  val Version = "1.0.0-SNAPSHOT"
+  val Version = "1.1.0"
   val ScalaVersion = "2.11.6"
   val ScalatraVersion = "2.4.0.RC1"
 
@@ -27,7 +27,7 @@ object KoauthsamplescalatraBuild extends Build {
         "javax.servlet" % "javax.servlet-api" % "3.1.0",
         "io.spray" %% "spray-client" % "1.3.3",
         "com.typesafe.akka" %% "akka-actor" % "2.3.11",
-        "com.hunorkovacs" %% "koauth" % "1.0.1-SNAPSHOT"
+        "com.hunorkovacs" %% "koauth" % "1.1.0"
       )
     )
   )
